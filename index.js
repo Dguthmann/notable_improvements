e28ea6c0f965514ae7cc49c43e9ae34ad5a0e612
David Guthmann <drguthmann@cox.net>
2020-10-09 14:36:17 -0500

    Working on post requests

--- index.js
+++ index.js
@@ -4,19 +4,44 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Build data structures
     // Array of notes 
-    const noteData = [];
-
+    let noteData = [
+        {
+            "id":0,
+            "title":"Test Title",
+            "text":"Test text"
+        }
+    ];
+    // ID counter
+    let index = noteData[0].id;
 // TODO: Build my routes
-    // TODO: Get route for notes data
-    // TODO: Get route for homepage
-    // TODO: POST route to add a note
-    // TODO: POST route to delete a note
+     
+// Get route for homepage
 app.get("/", function(req, res){
     res.send('basic structure')
 })
 
+// Get route for notes data
+app.get("/api/notes", function(req,res){
+    res.json(noteData);
+})
+
+// TODO: POST route to add a note
+app.post("/api/notes", function(req,res){
+    console.log(req.body);
+    noteData.push(req.body);
+    index++;
+    res.json(req.body);
+})
+// TODO: POST route to delete a note
+// app.de("/api/notes", function(req,res){
+//     res.json(noteData);
+// })
 
 app.listen(PORT, function(){
     console.log(`listening on port ${PORT}`);
