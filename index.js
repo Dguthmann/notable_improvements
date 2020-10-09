a90915c9e655532774d9db1e230d262e29913f60
David Guthmann <drguthmann@cox.net>
2020-10-09 14:20:56 -0500

    Basic Server Structure and data structure

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-// TODO: build server sturcture
-// TODO: build data structures
-    // TODO: Array of notes
+// Build server sturcture
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Build data structures
+    // Array of notes 
+    const noteData = [];
+
 // TODO: Build my routes
     // TODO: Get route for notes data
     // TODO: Get route for homepage
     // TODO: POST route to add a note
     // TODO: POST route to delete a note
+app.get("/", function(req, res){
+    res.send('basic structure')
+})
+
+
+app.listen(PORT, function(){
+    console.log(`listening on port ${PORT}`);
+})
