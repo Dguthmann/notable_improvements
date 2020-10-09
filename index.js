710ee9db52189542910ef9ed440d1e3202522505
David Guthmann <drguthmann@cox.net>
2020-10-09 14:15:28 -0500

    Basic Design Structure

---
+++ index.js
@@ -0,0 +1,8 @@
+// TODO: build server sturcture
+// TODO: build data structures
+    // TODO: Array of notes
+// TODO: Build my routes
+    // TODO: Get route for notes data
+    // TODO: Get route for homepage
+    // TODO: POST route to add a note
+    // TODO: POST route to delete a note
